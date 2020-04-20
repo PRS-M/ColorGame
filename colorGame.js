0d34f744f46a10792764ab30b61173cb33617747
Mateusz Prusinowski <prusinowski.mateusz@gmail.com>
2020-04-21 00:23:04 +0200

    Add randomColor() and generateRandomColors() for generating an array of random colors

--- colorGame.js
+++ colorGame.js
@@ -1,11 +1,4 @@
-var colors = [
-    "rgb(255, 0, 0)",
-    "rgb(255, 255, 0)",
-    "rgb(0, 255, 0)",
-    "rgb(0, 255, 255)",
-    "rgb(0, 0, 255)",
-    "rgb(255, 0, 255)"
-]
+var colors = generateRandomColors(6);
 
 var squares = document.querySelectorAll(".square");
 var pickedColor = pickColor();
@@ -33,7 +26,7 @@ for (var i = 0; i < squares.length; i++){
 }
 
 function changeColors(color){
-    for(var i = 0; i < squares.length; i++){
+    for (var i = 0; i < squares.length; i++){
         squares[i].style.backgroundColor = color;
     }
 }
@@ -41,4 +34,21 @@ function changeColors(color){
 function pickColor(){
     var random = Math.floor((Math.random() * colors.length));
     return colors[random];
+}
+
+function generateRandomColors(num){
+    var arr = [];
+    for (var i = 0; i < num; i++){
+        arr.push(randomColor());
+    }
+
+    return arr;
+}
+
+function randomColor(){
+    var r = Math.floor(Math.random() * 256);
+    var g = Math.floor(Math.random() * 256);
+    var b = Math.floor(Math.random() * 256);
+
+    return "rgb(" + r + ", " + g + ", " + b + ")"
 }
