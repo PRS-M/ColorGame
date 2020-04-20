8bbde64be2bcbfee7b4667e16789617ba666aeeb
Mateusz Prusinowski <prusinowski.mateusz@gmail.com>
2020-04-21 00:03:01 +0200

    Add func changeColors()

--- colorGame.js
+++ colorGame.js
@@ -23,10 +23,17 @@ for (var i = 0; i < squares.length; i++){
         var clickedColor = this.style.backgroundColor;
         if (clickedColor === pickedColor) {
             messageDisplay.textContent = "Correct!";
+            changeColors(clickedColor);
         }
         else {
             this.style.backgroundColor = "#232323";
             messageDisplay.textContent = "Try Again";
         }
     });
+}
+
+function changeColors(color){
+    for(var i = 0; i < squares.length; i++){
+        squares[i].style.backgroundColor = color;
+    }
 }
