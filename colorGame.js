64afa768233acd9256dc0e850898bbd99600887b
Mateusz Prusinowski <prusinowski.mateusz@gmail.com>
2020-04-21 00:12:30 +0200

    Add pickColor() func

--- colorGame.js
+++ colorGame.js
@@ -8,7 +8,7 @@ var colors = [
 ]
 
 var squares = document.querySelectorAll(".square");
-var pickedColor = colors[3];
+var pickedColor = pickColor();
 var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 
@@ -36,4 +36,9 @@ function changeColors(color){
     for(var i = 0; i < squares.length; i++){
         squares[i].style.backgroundColor = color;
     }
+}
+
+function pickColor(){
+    var random = Math.floor((Math.random() * colors.length));
+    return colors[random];
 }
