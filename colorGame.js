4c4bc91fa73f1d3cda04fdcfe889d724a1a20897
Mateusz Prusinowski <prusinowski.mateusz@gmail.com>
2020-04-25 00:32:51 +0200

    Add event listeners for easy and hard buttons

--- colorGame.js
+++ colorGame.js
@@ -6,6 +6,18 @@ var colorDisplay = document.getElementById("colorDisplay");
 var messageDisplay = document.querySelector("#message");
 var h1 = document.querySelector("h1");
 var resetButton = document.querySelector("#reset");
+var easyBtn = document.querySelector("#easyBtn");
+var hardBtn = document.querySelector("#hardBtn");
+
+easyBtn.addEventListener("click", function(){
+    easyBtn.classList.add("selected");
+    hardBtn.classList.remove("selected");
+});
+
+hardBtn.addEventListener("click", function(){
+    hardBtn.classList.add("selected");
+    easyBtn.classList.remove("selected");
+});
 
 resetButton.addEventListener("click", function(){
     colors = generateRandomColors(6);
