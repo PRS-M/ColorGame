22399a21e604f26a638b9a123e9ab9501cd18264
Mateusz Prusinowski <prusinowski.mateusz@gmail.com>
2020-04-25 14:14:10 +0200

    Refactor, add setup functions

--- colorGame.js
+++ colorGame.js
@@ -11,19 +11,27 @@ var modeButtons = document.querySelectorAll(".mode");
 
 init();
 
-function init(){
+function init() {
+    setupModeButtons();
+    setupSquares();
+    reset();
+}
+
+function setupModeButtons() {
     for (var i = 0; i < modeButtons.length; i++) {
         modeButtons[i].addEventListener("click", function () {
             modeButtons[0].classList.remove("selected");
             modeButtons[1].classList.remove("selected");
             this.classList.add("selected");
-    
+
             this.textContent === "Easy" ? numSquares = 3 : numSquares = 6;
-    
+
             reset();
         });
     }
+}
 
+function setupSquares() {
     for (var i = 0; i < squares.length; i++) {
         //Add click listeners
         squares[i].addEventListener("click", function () {
@@ -41,8 +49,6 @@ function init(){
             }
         });
     }
-
-    reset();
 }
 
 function reset() {
